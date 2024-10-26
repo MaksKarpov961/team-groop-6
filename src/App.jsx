4f528e02d81ee9c3c77821e67d94963c22efa01e
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-10-26 12:02:43 +0200

    task-4

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import {
 } from 'components';
 import article from 'data/article.json';
 import stats from 'data/stats.json';
+import transactions from 'data/transactions.json';
 
 // console.log('article', article);
 
@@ -32,7 +33,7 @@ export const App = () => {
         <Heading title="Task 3 Forbes list" top bottom />
         <ForbesList />
         <Heading title="Task 4 Crypto history" top bottom />
-        <CryptoHistory />
+        <CryptoHistory items={transactions} />
       </Container>
     </Section>
   );
